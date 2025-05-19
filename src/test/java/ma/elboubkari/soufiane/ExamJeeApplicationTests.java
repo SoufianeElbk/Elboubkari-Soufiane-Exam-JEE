604d0eaa6c5d82ad90af91c7e3d2b1ba2bcccfe1
Soufiane ELBOUBKARI <elboubkarisoufiane03@gmail.com>
2025-05-19 09:00:23 +0100

    Create Spring boot project

---
+++ src/test/java/ma/elboubkari/soufiane/ExamJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.elboubkari.soufiane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamJeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
